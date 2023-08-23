f41f06d1152a4b3b628f133f48c031eaeb342fcf
joaovsa97 <joaos.azevedo10@gmail.com>
2023-08-23 18:56:29 -0300

    feat: url fix

--- next.config.js
+++ next.config.js
@@ -3,16 +3,20 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-// module.exports = nextConfig
+module.exports = nextConfig
 
-module.exports = {
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/socket.io/:path*",
-        destination: "http://localhost:8080/socket.io/:path*",
-      },
-    ];
-  },
-};
+// module.exports = {
+//   reactStrictMode: true,
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/socket.io/:path*",
+//         destination: "http://localhost:8080/socket.io/:path*",
+//       },
+//       {
+//         source: "/",
+//         destination: "https://nextjs-socketio-api.vercel.app/"
+//       }
+//     ];
+//   },
+// };
--- src/components/chatComponent.jsx
+++ src/components/chatComponent.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080");
+const socket = io("https://nextjs-socketio-api.vercel.app/");
 
 export default function Chat() {
   const [message, setMessage] = useState("");
